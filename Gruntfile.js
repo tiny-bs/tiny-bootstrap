bc51c170b7c196c9ac0ddf38fe00ada24603b549
MC <mr.moriarity@gmail.com>
2014-05-14 00:23:22 +0200

    initial project setup

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: ['js/libs/*.js', 'js.*'],
+        dest: 'js/build/<%= pkg.name %>.min.js'
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'css',
+          cssDir: 'css'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['compass']
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'watch']);
+};
